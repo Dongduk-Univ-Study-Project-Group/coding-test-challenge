c17ef47a79472b277d44555f4e0325650e44a028
JaeHyeonSim <peanutandnuts@gmail.com>
2023-01-15 21:18:51 +0900

    fix: 코드 수정

--- JaeHyeonSim/개인정보 수집 유효기간/solution.py
+++ JaeHyeonSim/개인정보 수집 유효기간/solution.py
@@ -6,12 +6,10 @@ def solution(today, terms, privacies):
     
     t = today.split(".")    # 오늘 날짜 str to date
     tDate = dt.datetime(int(t[0]), int(t[1]), int(t[2]))
-    print(tDate)
     
     termDict = {}           # 약관 종류별 유효기간
     for term in terms:
         termDict[term.split()[0]] = int(term.split()[1])
-    print(termDict)
     
     index = 1
     for privacie in privacies:
@@ -23,7 +21,7 @@ def solution(today, terms, privacies):
         
         month += termDict.get(p[3])     # 약관에 맞는 기간을 더한다
         if month > 12:                  # 12월을 넘어갔을 때 year, month 처리
-            year += (month - 12) // 12
+            year += month // 12
             month = (month - 12) % 12
         
         pDate = dt.datetime(year, month, day)   # 만료일과 날짜 비교
