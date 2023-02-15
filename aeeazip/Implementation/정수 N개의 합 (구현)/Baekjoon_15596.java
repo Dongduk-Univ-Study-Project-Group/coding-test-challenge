f583c4d3c891a242fbe31e8958704b04363e0545
aeeazip <codnjs0809@naver.com>
2023-02-15 13:46:31 +0900

    feat : 코딩 테스트 챌린지 34일차

---
+++ aeeazip/Implementation/정수 N개의 합 (구현)/Baekjoon_15596.java
@@ -0,0 +1,14 @@
+package baekjoon;
+
+import java.util.*;
+
+public class Test {
+    long sum(int[] a) {
+        long ans = 0;
+        
+        for(int i=0; i<a.length; i++)
+            ans += arr[i];
+        
+        return ans;
+    }
+}
