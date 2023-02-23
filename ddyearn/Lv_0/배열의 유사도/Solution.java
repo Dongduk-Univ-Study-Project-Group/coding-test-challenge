cd93172ed28dfe126fed7cf7e2d98d623b9ac373
ddyearn <bestsy777@gmail.com>
2023-02-23 19:32:23 +0900

    [유승연] 48일차: 배열의 유사도

---
+++ ddyearn/Lv_0/배열의 유사도/Solution.java
@@ -0,0 +1,13 @@
+import java.util.Arrays;
+
+class Solution {
+    public int solution(String[] s1, String[] s2) {
+        int answer = 0;
+        for(String s: s1) {
+            if(Arrays.asList(s2).contains(s)) {
+                answer++;
+            }
+        }
+        return answer;
+    }
+}
